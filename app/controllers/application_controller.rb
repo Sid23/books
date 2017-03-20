a1ab134763d3adb4708faf8eb9ac6a688ce9ab4a
Sid23 <marchisio.simone@gmail.com>
2017-03-20 11:58:59 +0100

    Destroy done, refinement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def show
-    @book = Book.find(params[:id])
-    if @book
-  end
 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController < ActionController
+class BooksController < ApplicationController
   before_action :set_book, only: [:edit, :update, :show, :destroy]
 
   def index
@@ -6,42 +6,50 @@ class BooksController < ActionController
   end
 
   def new
-    #@book = Book.new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      sd
-    else
-      render 'new'
-    end
-  end
-
-  def new
     @book = Book.new
   end
 
-  def show
-  end
+   def create
+     @book = Book.new(book_params)
+     if @book.save
+       flash[:notice] = "Book created successfully !!"
+       redirect_to book_path(@book)
+     else
+       render 'new'
+     end
+   end
+
+ def show
+
+ end
 
   def edit
+
   end
 
   def update
-    
+    #pass the list of updated params
+    if @book.update(book_params)
+       flash[:notice] = "Book updated successfully !!"
+       redirect_to book_path(@book)
+     else
+       render 'edit'
+     end
   end
 
   def destroy
-    
+    @book.destroy
+    flash[:notice] = "Book deleted successfully"
+    redirect_to books_path()
   end
 
   private
     def set_book
-      @book.find(params[:id])
+      @img = "books/b" + params[:id].to_s + ".jpg"
+      @book = Book.find(params[:id])
     end
 
     def book_params
-      params.require(:book).permit(:title, :author, :description)
+      params.require(:book).permit(:title, :author, :year)
     end
 end
